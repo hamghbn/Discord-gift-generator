3a97cdbed45f8e82f67d9af2604aedb6285c41cf
hamghbn <48649729+hamghbn@users.noreply.github.com>
2020-11-22 11:41:47 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,52 @@
+const request = require('request');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const generator = require('generate-password');
+var fs = require('fs');
+
+client.login("Nzc5OTM2MjgyOTY4MTk1MDcz.X7nyRA.28WCDvgMSAXZ12Biosi5HTp16Q0"); 
+
+var i = 0;
+var i1 = 0;
+var c = 0;
+var arr = [];
+
+
 
 client.on('ready', () => {
-  var i = 0;
-    while (i<1000) { //Number of messages
-var password = generator.generate({
-    length: 16,
-    numbers: true
-    });
- client.channels.get('Id').send("https://discord.gift/" + password); //Channel ID
- i++;
-}
- });
- 
-client.login('token'); //Bot Token 
+	a();
+	function a() {
+		console.clear();
+		var id = generator.generate({
+    		length: 16,
+    		numbers: true
+		})
+		request({url: `https://discordapp.com/api/v6/entitlements/gift-codes/${id}`, json: true}, function(err, res, json) {
+			i++;
+	
+			if (json.code === id){
+				c++;
+	 			fs.appendFileSync("gifts.txt", "\nhttps://discord.gift/"+id);
+	 			arr.push(json.code);
+	 			fs.appendFileSync("log.txt", "\n"+json);
+	 			a();
+	 			channel = client.channels.cache.get('779963256411652097');
+    	 		channel.send("https://discord.gift/"+id);
+			}
+			else{
+				if (json.message === 'You are being rate limited.'){
+					setTimeout(a, 60000); console.log('waiting...');
+				}
+				else{
+					a(); 
+					console.log(c + ' of ' + i);
+					channel = client.channels.cache.get('779963256411652097');
+					channel.send('X' + c + ' of ' + i );
+					for (i1 = 0; i1 < arr.length; i1){ 
+						console.log(arr[i1]);
+					}
+				}
+			}
+		})
+	}
+});
